5f68ac1c066fa43c170f68df532ca9e2a97f6791
MJSasha <sasha.ushankov@gmail.com>
2022-03-07 18:04:34 +0300

    It works!

--- src/main/java/com/hiber/HibernateLesson.java
+++ src/main/java/com/hiber/HibernateLesson.java
@@ -1,19 +1,35 @@
 package com.hiber;
 
+import com.hiber.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
+
+import java.time.LocalDate;
 
 public class HibernateLesson {
 
     public static void main(String[] args) {
 
         Configuration configuration = new Configuration();
+        configuration.addAnnotatedClass(User.class);
         configuration.configure();
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()){
-            System.out.println("Ok");
+            session.beginTransaction();
+
+            User user = User.builder()
+                    .username("sasha@gmail.com")
+                    .firstname("sasha")
+                    .lastname("ushankov")
+                    .birthDate(LocalDate.of(2003, 3, 16))
+                    .age(18)
+                    .build();
+            session.save(user);
+
+            session.getTransaction().commit();
         }
     }
 }
---
+++ src/main/java/com/hiber/entity/User.java
@@ -0,0 +1,29 @@
+package com.hiber.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    private String username;
+    private String firstname;
+    private String lastname;
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+    private Integer age;
+}
